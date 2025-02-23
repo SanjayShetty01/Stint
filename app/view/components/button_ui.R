91d404fb4386d79f944180d67654382e07c8d8c1
shettysanjay01 <shettysanjay01@gmail.com>
2025-02-23 22:58:02 +0530

    ui components

---
+++ app/view/components/button_ui.R
@@ -0,0 +1,11 @@
+box::use(shinyWidgets, htmltools)
+
+button_ui <- function(id, text) {
+  shinyWidgets::actionBttn(
+    inputId = id,
+    label = text,
+    style = "material-flat",
+    color = "danger"
+  ) |>
+    htmltools::tagAppendAttributes(style = "width: inherit;")
+}
